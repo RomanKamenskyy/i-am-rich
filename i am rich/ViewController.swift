0261d4b649b1794fabe336f6ce6b3c5d7d1d8ee8
kamensky roman <kamensky.ra@gmail.com>
2021-09-12 20:35:48 +0300

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Роман Каменский on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
